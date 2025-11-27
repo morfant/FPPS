63ca5b589bd1eb9ed172a4b21fd4afb5d374ac7e
giy-macbookpro <giy.hands@gmail.com>
2025-11-28 00:21:58 +0900

    1128 수업준비 oF

--- 2025/FALL/week_10_test/src/ofApp.cpp
+++ 2025/FALL/week_10_test/src/ofApp.cpp
@@ -1,7 +1,8 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup()
+{
 
     width = ofGetWidth();
     height = ofGetHeight();
@@ -13,110 +14,98 @@ void ofApp::setup(){
     mx = -1;
     my = -1;
 
-}
-
-//--------------------------------------------------------------
-void ofApp::update(){
-
-    if (ofGetMousePressed())
+    for (int i = 0; i < 50; i++)
     {
-        mx = ofGetMouseX();
-        my = ofGetMouseY();
+        circles[i] = Circle(width * 0.5, height * 0.5, 100);
     }
-
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::update()
+{
+    for (int i = 0; i < 50; i++)
+    {
+        if (ofGetMousePressed())
+        {
+            circles[i].setRadius(ofRandom(40, 120));
+        }
+        circles[i].move();
+    }
+}
 
-    ofSetColor(255, 0, 0);
-    ofNoFill();
-    ofDrawCircle(100, 100, 100);
+//--------------------------------------------------------------
+void ofApp::draw()
+{
 
-    ofFill();
-    ofSetColor(0, 0, 255);
-    ofDrawRectangle(300, 300, 100, 100);
-
-    ofSetColor(0, 200, 100);
-    ofDrawLine(0, 0, width, height);
-
-    ofSetColor(0, 100, 200);
-    ofDrawLine(0, height, width, 0);
+    for (int i = 0; i < 50; i++)
+    {
+        circles[i].show();
+    }
 
     ofDrawBitmapStringHighlight("FPS: " + ofToString(ofGetFrameRate(), 2), 10, 20);
-
-    if (mx != -1 && my != -1)
-    {
-        ofSetColor(255, 100, 100);
-        ofDrawCircle(mx, my, 10);
-    }
-
-    
-
-
 }
 
 //--------------------------------------------------------------
-void ofApp::exit(){
-
+void ofApp::exit()
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
+void ofApp::keyPressed(int key)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
+void ofApp::keyReleased(int key)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
+void ofApp::mouseMoved(int x, int y)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
+void ofApp::mouseDragged(int x, int y, int button)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
+void ofApp::mousePressed(int x, int y, int button)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
+void ofApp::mouseReleased(int x, int y, int button)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY){
-
+void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
+void ofApp::mouseEntered(int x, int y)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
+void ofApp::mouseExited(int x, int y)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
+void ofApp::windowResized(int w, int h)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
+void ofApp::gotMessage(ofMessage msg)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo)
+{
 }
--- 2025/FALL/week_10_test/src/ofApp.h
+++ 2025/FALL/week_10_test/src/ofApp.h
@@ -2,29 +2,135 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+class Shape
+{
+protected:
+	ofColor col;
+	ofColor colTouched;
 
-	public:
-		void setup() override;
-		void update() override;
-		void draw() override;
-		void exit() override;
+public:
+	Shape() {};
 
-		void keyPressed(int key) override;
-		void keyReleased(int key) override;
-		void mouseMoved(int x, int y ) override;
-		void mouseDragged(int x, int y, int button) override;
-		void mousePressed(int x, int y, int button) override;
-		void mouseReleased(int x, int y, int button) override;
-		void mouseScrolled(int x, int y, float scrollX, float scrollY) override;
-		void mouseEntered(int x, int y) override;
-		void mouseExited(int x, int y) override;
-		void windowResized(int w, int h) override;
-		void dragEvent(ofDragInfo dragInfo) override;
-		void gotMessage(ofMessage msg) override;
+	void hide()
+	{
+		col.a = 0;
+	}
+
+	void unHide()
+	{
+		col.a = 100;
+	}
+};
+
+class Circle : public Shape
+{
+
+private:
+	// member variables
+	float posX, posY;
+	float speedX, speedY;
+	float radius;
+
+public:
+	// Constructor we will use
+	Circle(float x, float y, float r)
+	{
+		posX = x;
+		posY = y;
+		radius = r;
+
+		speedX = ofRandom(-3, 3);
+		speedY = ofRandom(-3, 3);
+
+		col = ofColor(0, 100, ofRandom(120), 100);
+		colTouched = ofColor(0, 100, 120, 255);
+	}
+
+	// Default constructor
+	Circle() {};
+
+	// member functions
+	void setRadius(float rad)
+	{
+		radius = rad;
+	}
+
+	void move()
+	{
+		posX = posX + speedX;
+		posY = posY + speedY;
 
 
-		int width, height;
-		int mx, my;
-		
+		if (posX - radius <= 0 || posX + radius >= ofGetWidth())
+		{
+			speedX = speedX * -1;
+		}
+
+		if (posY - radius <= 0 || posY + radius >= ofGetHeight())
+		{
+			speedY = speedY * -1;
+		}
+
+	}
+
+
+	void show()
+	{
+		if (isMouseTouched() == true)
+		{
+			// ofSetColor(colTouched);
+			hide();
+		}
+		else
+		{
+			unHide();
+		}
+
+		ofSetColor(col);
+		ofFill();
+		ofDrawCircle(posX, posY, radius);
+	};
+
+	bool isMouseTouched()
+	{
+		float dist = ofDist(posX, posY, ofGetMouseX(), ofGetMouseY());
+
+		if (dist <= radius)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+};
+
+class ofApp : public ofBaseApp
+{
+
+public:
+	void setup() override;
+	void update() override;
+	void draw() override;
+	void exit() override;
+
+	void keyPressed(int key) override;
+	void keyReleased(int key) override;
+	void mouseMoved(int x, int y) override;
+	void mouseDragged(int x, int y, int button) override;
+	void mousePressed(int x, int y, int button) override;
+	void mouseReleased(int x, int y, int button) override;
+	void mouseScrolled(int x, int y, float scrollX, float scrollY) override;
+	void mouseEntered(int x, int y) override;
+	void mouseExited(int x, int y) override;
+	void windowResized(int w, int h) override;
+	void dragEvent(ofDragInfo dragInfo) override;
+	void gotMessage(ofMessage msg) override;
+
+	int width, height;
+	int mx, my;
+
+	// Circle cir1;
+	Circle circles[50];
 };
